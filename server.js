f62bc4373a98f6f264a04cc8d6c01eb1c9fa6d82
ify1237ibiam <ifegwuifeanyi2021@gmail.com>
2025-03-03 17:55:11 -0800

    Added project files

--- server.js
+++ server.js
@@ -6,14 +6,29 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+// Route for the home/index page
+app.get("/", function(req, res){
+  console.log("Home route accessed");
+  res.render("index", {title: "Home"});
+});
 
 /* ***********************
  * Local Server Information
