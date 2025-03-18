db0aadc183ea55dc9d3ffa8840c74efb19df2aef
ify1237ibiam <ifegwuifeanyi2021@gmail.com>
2025-03-18 11:25:14 +0100

    ALL ASSIGNMENTS DONE

--- server.js
+++ server.js
@@ -1,60 +1,53 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * My, This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const baseController = require("./controllers/baseController");
+const static = require("./routes/static");
+const inventoryRoute = require("./routes/inventoryRoute"); // 
 
-const expressLayouts = require("express-ejs-layouts")
-
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-
-
+const app = express();
 
 /* ***********************
  * Serve Static Files
-this will serve our image
+ * This will serve our images
  *************************/
-app.use(express.static("public")); 
-
-
-
+app.use(express.static("public"));
 
 /* ***********************
  * View Engine and Templates
  *************************/
-
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
-
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // Not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
+// Inventory routes
+app.use("/inv", inventoryRoute); // 
 
 // Route for the home/index page
-app.get("/", function(req, res){
-  console.log("Home route accessed");
-  res.render("index", {title: "Home"});
-});
-
+app.get("/", baseController.buildHome);
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500; // 
+const host = process.env.HOST || "localhost"; 
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
